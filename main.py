34e07ef2d7d55ed26778b37877fbc57b64fc2cb5
Nolan essertaize <n.essertaize@xefi.fr>
2024-09-19 16:07:11 +0200

    Update db

--- main.py
+++ main.py
@@ -10,22 +10,17 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 class Item(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    tag: str | None = Field(default=None)
-    nom: str | None = Field(default=None)
-    description: str | None = Field(default=None)
-    deadline: str | None = Field(default=None)
-    createdtime: str | None = Field(default=None)
+    tag: str
+    nom: str
+    description: str
+    deadline: str
+    createdtime: str
+    bin: str
 
 sqlite_file_name = "db.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
-
-def select_item():
-    with Session(engine) as session:
-        statement = select(Item)
-        results = session.exec(statement)
-        return results
 
 #DataBase usage
 app = FastAPI()
@@ -41,44 +36,64 @@ class TodoList:
         self.Date = datetime.now().strftime("%Y-%m-%d")
         self.Deadline = deadline
 
-# Simulating a database with a list
-todo_list = []
-
-# Simulating a bin
-bin_list = []
-
 @app.get("/")
 async def home(request: Request):
-    results = select_item()
-    return templates.TemplateResponse("index.html", {"request": request, "todo_list": todo_list, "results": results})
+    with Session(engine) as session:
+        statement = select(Item).where(Item.bin == "False")
+        results = session.exec(statement)
+        db = []
+        for item in results:
+            db.append(item)
+    return templates.TemplateResponse("index.html", {"request": request, "db": db})
 
 @app.post("/add")
-async def add(todo: str = Form("todo"), deadline: str = Form("deadline")):
-    new_item = TodoList(todo, deadline)
-    todo_list.append(new_item)
+async def add(tag: str = Form("tag"), todo: str = Form("todo"), deadline: str = Form("deadline")):
+    new_item = Item(tag=tag, nom=todo, deadline=deadline, createdtime=datetime.now().strftime("%Y-%m-%d"), bin="False")
+    with Session(engine) as session:
+        session.add(new_item)
+        session.commit()
     return RedirectResponse(url="/", status_code=303)
 
-@app.get("/delete/{todo_id}")
-async def delete(todo_id: int):
-    if 0 <= todo_id < len(todo_list):
-        bin_list.append(todo_list[todo_id])
-        del todo_list[todo_id]
+@app.get("/delete/{item_id}")
+async def delete(item_id: int):
+    with Session(engine) as session:
+        statement = select(Item).where(Item.id == item_id)
+        results = session.exec(statement)
+        item = results.one()
+        item.bin = "True"
+        session.commit()
+        session.refresh(item)
     return RedirectResponse(url="/", status_code=303)
 
 @app.get("/bin")
 async def show_bin(request: Request):
-    return templates.TemplateResponse("bin.html", {"request": request, "bin_list": bin_list})
+    with Session(engine) as session:
+        statement = select(Item).where(Item.bin == "True")
+        results = session.exec(statement)
+        db = []
+        for item in results:
+            db.append(item)
+    return templates.TemplateResponse("bin.html", {"request": request, "db": db})
 
-@app.get("/restaure/{bin_id}")
-async def restaure(bin_id: int):
-    if 0 <= bin_id < len(bin_list):
-        todo_list.append(bin_list[bin_id])
-        del bin_list[bin_id]
+
+@app.get("/restaure/{item_id}")
+async def restaure(item_id: int):
+    with Session(engine) as session:
+        statement = select(Item).where(Item.id == item_id)
+        results = session.exec(statement)
+        item = results.one()
+        item.bin = "False"
+        session.commit()
+        session.refresh(item)
     return RedirectResponse(url="/", status_code=303)
 
-@app.get("/deletetrash/{bin_id}")
-async def deletetrash(bin_id: int):
-    if 0 <= bin_id < len(bin_list):
-        del bin_list[bin_id]
+@app.get("/deletetrash/{item_id}")
+async def deletetrash(item_id: int):
+    with Session(engine) as session:
+        statement = select(Item).where(Item.id == item_id)
+        results = session.exec(statement)
+        item_db = results.first()
+        session.delete(item_db)
+        session.commit()
     return RedirectResponse(url="/bin", status_code=303)
 
--- test.py
+++ test.py
@@ -1,24 +1,27 @@
-from typing import Optional
-from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-#DataBase usage
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 class Item(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    tag: str | None = Field(default=None)
-    nom: str | None = Field(default=None)
-    description: str | None = Field(default=None)
-    deadline: str | None = Field(default=None)
-    createdtime: str | None = Field(default=None)
+    tag: str
+    nom: str
+    description: str
+    deadline: str
+    createdtime: str
+    bin: bool
 
 sqlite_file_name = "db.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
 
-def select_item():
+def test_bdd():
     with Session(engine) as session:
-        results = session.exec(select(Item)).all()
-        print(f"\nResults : {results}\n")
+        statement = select(Item).where(Item.bin == "True")
+        results = session.exec(statement)
+        db = []
+        for item in results:
+            db.append(item)
+            print("\n Item : ",item)
 
-select_item()
+test_bdd()
