efc17ac82530b2be21b273e80cf9bde1e9812e8a
Nolan essertaize <n.essertaize@xefi.fr>
2024-09-24 10:40:09 +0200

    Add login and registration

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-from fastapi import FastAPI, Request, Form, HTTPException, status
+from email.policy import default
+from tkinter.font import names
+import hashlib
+
+from certifi import where
+from fastapi import FastAPI, Request, Form, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jinja2.ext import loopcontrols
+from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from datetime import datetime
-from typing import Annotated
+from typing import Annotated, Union
 
+from markdown_it.rules_block import table
+from sqlalchemy import result_tuple
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
+#Token
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class User(BaseModel):
+    id: int
+    username: str
+    disabled: Union[bool, None] = None
+
+def decode_token(token):
+    return User(
+        username=token, email="john@example.com", full_name="John Doe"
+    )
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = decode_token(token)
+    return user
+
 #DataBase
-class Item(SQLModel, table=True):
+class Todousers(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    login: str
+    password: str
+
+class Todolist(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    id_user: int | None = Field(default=None, foreign_key="todousers.id")
     tag: str
-    nom: str
+    name: str
     description: str
     deadline: str
     createdtime: str
@@ -27,7 +60,7 @@ templates = Jinja2Templates(directory="templates")
 #Automation
 def updating_bin_status_with_id(id: int, changes: str) -> None:
     with Session(engine) as session:
-        statement = select(Item).where(Item.id == id)
+        statement = select(Todolist).where(Todolist.id == id and Todolist.id_user == 1)
         results = session.exec(statement)
         item = results.one()
         item.bin = changes
@@ -36,7 +69,7 @@ def updating_bin_status_with_id(id: int, changes: str) -> None:
 
 def updating_description_with_id(id: int, changes: str) -> None:
     with Session(engine) as session:
-        statement = select(Item).where(Item.id == id)
+        statement = select(Todolist).where(Todolist.id == id and Todolist.id_user == 1)
         results = session.exec(statement)
         item = results.one()
         item.description = changes
@@ -45,7 +78,7 @@ def updating_description_with_id(id: int, changes: str) -> None:
 
 def deleting_with_id(id: int) -> None:
     with Session(engine) as session:
-        statement = select(Item).where(Item.id == id)
+        statement = select(Todolist).where(Todolist.id == id and Todolist.id_user == 1)
         results = session.exec(statement)
         item_db = results.first()
         session.delete(item_db)
@@ -54,7 +87,7 @@ def deleting_with_id(id: int) -> None:
 def updating_tag_with_id(id: int, changes: str) -> None:
     if changes != "":
         with Session(engine) as session:
-            statement = select(Item).where(Item.id == id)
+            statement = select(Todolist).where(Todolist.id == id and Todolist.id_user == 1)
             results = session.exec(statement)
             item = results.one()
             tag = ["Todo", "Doing", "Done"]
@@ -69,21 +102,63 @@ def updating_tag_with_id(id: int, changes: str) -> None:
                 print("error")
             session.commit()
             session.refresh(item)
+
+def checking_users(loginuser:str) -> bool:
+    with Session(engine) as session:
+        statement = select(Todousers).where(Todousers.login == loginuser)
+        results = session.exec(statement)
+        loginlist = []
+        for login in results:
+            loginlist.append(login)
+
+        if loginuser in loginlist:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Name already taken",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        else:
+            return True
 #Route
 @app.get("/")
 async def home(request: Request):
     with Session(engine) as session:
-        statement = select(Item).where(Item.bin == "False")
+        statement = select(Todolist).where(Todolist.bin == "False")
         results = session.exec(statement)
         db = []
         for item in results:
             db.append(item)
     return templates.TemplateResponse("index.html", {"request": request, "db": db})
 
+@app.get("/connexion")
+async def connexion(request: Request):
+    return templates.TemplateResponse("connexion.html", {"request": request})
+
+@app.post("/login")
+async def add(name: str = Form("username"), password: str = Form("password")):
+    with Session(engine) as session:
+        statement = select(Todousers).where(Todousers.login == name)
+        results = session.exec(statement).all()
+        print(f"\n Commentaire {results}\nFor username : {name}")
+        hash_password = hashlib.sha256(b"password").hexdigest()
+        for user in results:
+            if user.password != hash_password:
+                raise HTTPException(status_code=400, detail="Incorrect password")
+        else:
+            return RedirectResponse(url="/", status_code=303)
+
+@app.post("/register")
+async def add(name: str = Form("name"), password: str = Form("password")):
+    hash_password = hashlib.sha256(b"password").hexdigest()
+    new_user = Todousers(login= name, password=hash_password)
+    with Session(engine) as session:
+        session.add(new_user)
+        session.commit()
+    return RedirectResponse(url="/connexion", status_code=303)
 
 @app.post("/add")
 async def add(tag: str = Form("tag"), todo: str = Form("todo"), deadline: str = Form("deadline")):
-    new_item = Item(tag=tag, nom=todo, deadline=deadline, createdtime=datetime.now().strftime("%Y-%m-%d"), bin="False")
+    new_item = Todolist(id_user= 1, tag=tag, name=todo, deadline=deadline, createdtime=datetime.now().strftime("%Y-%m-%d"), bin="False")
     with Session(engine) as session:
         session.add(new_item)
         session.commit()
@@ -98,7 +173,7 @@ async def delete(item_id: int):
 @app.get("/bin")
 async def show_bin(request: Request):
     with Session(engine) as session:
-        statement = select(Item).where(Item.bin == "True")
+        statement = select(Todolist).where(Todolist.bin == "True" and Todolist.id_user == 1)
         results = session.exec(statement)
         db = []
         for item in results:
--- test.py
+++ test.py
@@ -1,27 +1,94 @@
+from typing import Annotated, Union
 
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
-class Item(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    tag: str
-    nom: str
-    description: str
-    deadline: str
-    createdtime: str
-    bin: bool
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
-sqlite_file_name = "db.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+app = FastAPI()
 
-engine = create_engine(sqlite_url, echo=True)
 
-def test_bdd():
-    with Session(engine) as session:
-        statement = select(Item).where(Item.bin == "True")
-        results = session.exec(statement)
-        db = []
-        for item in results:
-            db.append(item)
-            print("\n Item : ",item)
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
-test_bdd()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
